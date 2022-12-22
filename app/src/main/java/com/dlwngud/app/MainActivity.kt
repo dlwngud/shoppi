bca81650b546f58c8a4ec5d55372bfbf001a138d
dlwngud <wngud980411@naver.com>
2022-12-23 00:57:01 +0900

    bottom navigation 구현 itemIconTintList : 기본색이 아닌 원래색을 사용가능하게 함

--- app/src/main/java/com/dlwngud/app/MainActivity.kt
+++ app/src/main/java/com/dlwngud/app/MainActivity.kt
@@ -2,18 +2,21 @@ package com.dlwngud.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.dlwngud.app.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d("TAG", "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        // 기본 color를 참조하지 않고 원래 색으로 표시
+        binding.navigationMain.itemIconTintList = null
     }
 
 }
