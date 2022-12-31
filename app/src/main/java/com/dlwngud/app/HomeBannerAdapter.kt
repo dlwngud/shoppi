c008dc278e8bfe771466fc6369482cdd4d40f12b
dlwngud <wngud980411@naver.com>
2023-01-01 00:10:15 +0900

    홈화면 배너 상세내용 표시

--- app/src/main/java/com/dlwngud/app/HomeBannerAdapter.kt
+++ app/src/main/java/com/dlwngud/app/HomeBannerAdapter.kt
@@ -1,14 +1,21 @@
 package com.dlwngud.app
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.dlwngud.app.databinding.FragmentHomeBinding
 import com.dlwngud.app.databinding.ItemHomeBannerBinding
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class HomeBannerAdapter :
     ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(BannerDiffCallback()) {
@@ -28,7 +35,37 @@ class HomeBannerAdapter :
     class HomeBannerViewHolder(private val binding: ItemHomeBannerBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(banner: Banner) {
-            Glide.with(itemView).load(banner.backgroundImageUrl).into(binding.ivBannerImage)
+            loadImage(banner.backgroundImageUrl, binding.ivBannerImage)
+
+            binding.tvBannerBadge.text = banner.badge.label
+            binding.tvBannerBadge.background =
+                ColorDrawable(Color.parseColor(banner.badge.backgroundColor))
+            binding.tvBannerTitle.text = banner.label
+
+            loadImage(banner.productDetail.thumbnailImageUrl, binding.ivBannerDetailThumbnail)
+            binding.tvBannerDetailBrandLabel.text = banner.productDetail.brandName
+            binding.tvBannerDetailProductLabel.text = banner.productDetail.label
+            binding.tvBannerDetailProductDiscountRate.text = "${banner.productDetail.discountRate}%"
+            calculateDiscountAmount(
+                binding.tvBannerDetailProductDiscountPrice,
+                banner.productDetail.discountRate,
+                banner.productDetail.price
+            )
+            applyPriceFormat(binding.tvBannerDetailProductPrice, banner.productDetail.price)
+        }
+
+        private fun calculateDiscountAmount(view: TextView, discountRate: Int, price: Int) {
+            val discountPrice = (price * (100 - discountRate) / 100.0).roundToInt()
+            applyPriceFormat(view, discountPrice)
+        }
+
+        private fun applyPriceFormat(view: TextView, price: Int) {
+            val decimalFormat = DecimalFormat("#,###")
+            view.text = decimalFormat.format(price) + "원"
+        }
+
+        private fun loadImage(urlString: String, imageView: ImageView) {
+            Glide.with(itemView).load(urlString).into(imageView)
         }
     }
 }
