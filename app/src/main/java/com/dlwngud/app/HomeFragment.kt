d6ec345620b3177bb40c1601ea7af1cf9771be2d
dlwngud <wngud980411@naver.com>
2023-01-02 22:36:22 +0900

    ViewPager이용해여 배너 만들기 Indicator사용

--- app/src/main/java/com/dlwngud/app/HomeFragment.kt
+++ app/src/main/java/com/dlwngud/app/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.dlwngud.app.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 import org.json.JSONObject
 
@@ -46,6 +47,19 @@ class HomeFragment: Fragment() {
             binding.vpHomeBanner.adapter = HomeBannerAdapter().apply {
                 submitList(homeData.topBanner)
             }
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+
+            binding.vpHomeBanner.offscreenPageLimit = 3
+            binding.vpHomeBanner.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(binding.vpHomeBannerIndicator, binding.vpHomeBanner) { tab, position ->
+
+            }.attach()
         }
     }
 }
